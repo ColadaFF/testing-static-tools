43434f35a7eabed938742a7fca45925bfd57af2c
Cristian Barrientos Montoya <cbarrientos@ias.com.co>
2019-07-13 11:02:06 -0500

    Cosas ...

---
+++ src/test-flow.js
@@ -0,0 +1,23 @@
+// @flow
+
+function add(a: number, b: number) {
+  return a + b;
+}
+
+type User = {
+  name: {
+    first: string,
+    last: string
+  }
+};
+function getFullName(user: User): string {
+  const {
+    name: { first, last }
+  } = user;
+
+  return [first, last].filter(Boolean).join();
+}
+
+getFullName({ name: { first: "Cristian", last: "Bar" } });
+
+add(1, 2);
